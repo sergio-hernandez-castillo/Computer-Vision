b7c6ee7c995c6bd9062f91f2f630c25ba6ecaf10
Sergio Hernandez <shc010013@gmail.com>
2022-06-03 15:04:25 -0500

    Removed comments

--- Assignments/Project/detect_vid.py
+++ Assignments/Project/detect_vid.py
@@ -9,7 +9,8 @@ frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 
 # create the `VideoWriter()` object
-out = cv2.VideoWriter('output/video_result_1.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, (frame_width, frame_height))
+out = cv2.VideoWriter('output/video_result_1.mp4',
+                      cv2.VideoWriter_fourcc(*'mp4v'), 30, (frame_width, frame_height))
 
 # detect objects in each frame of the video
 while cap.isOpened():
@@ -17,10 +18,10 @@ while cap.isOpened():
     if ret:
         image = frame
 
-        # cv2.imshow('image', image)
+        cv2.imshow('image', image)
         out.write(image)
-        # if cv2.waitKey(10) & 0xFF == ord('q'):
-        #     break
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
     else:
         break
 
